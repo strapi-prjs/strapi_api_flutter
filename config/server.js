df20c46f5fa55c4689ef719404ae8bf41412015a
philipszdavido <kurtwanger40@gmail.com>
2021-06-01 13:17:36 +0100

    Made some changes

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b9288f3377894b83a4242495b91857b4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0b84e612-bbc6-47bb-976d-f76c426f8811'
+};
